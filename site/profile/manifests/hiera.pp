5f45ba858d631f4ee0ed16502b8f969aec88373b
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-10-16 08:42:25 +0800

    Added datadir

--- site/profile/manifests/hiera.pp
+++ site/profile/manifests/hiera.pp
@@ -1,5 +1,6 @@
 class profile::hiera {
   class { '::hiera':
+    datadir   => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
     hierarchy => [
       '%{environment}',
       'common',
