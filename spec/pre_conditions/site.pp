694e05d0c8990ad54dc6889295212321b1566c45
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-11-06 10:34:26 +1100

    Adding windows tests and changed pre_conditions

--- spec/pre_conditions/site.pp
+++ spec/pre_conditions/site.pp
@@ -1,7 +1,12 @@
-#node 'centos6a.pdx.puppetlabs.demo' {
-  service { 'pe-puppetserver':
-    ensure => running,
-  }
-  #}
+# We are not going to actually have this service anywhere on our servers but
+# our code needs to refresh it. This is to trck puppet into doing nothing
+service { 'pe-puppetserver':
+  ensure     => 'running',
+  enable     => false,
+  hasrestart => false, # Force Puppet to use start and stop to restart
+  start      => 'ls', # This will always work
+  stop       => 'ls', # This will also always work
+  hasstatus  => false, # Force puppet to use our command for status
+  status     => 'ls' # This will always exit 0 and therefor Puppet will think the service is running
+}
 
-#node default {}
