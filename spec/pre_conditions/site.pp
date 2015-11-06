239d0b26a688b2565313b9ba4494a96f77ed034e
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-11-06 13:16:19 +1100

    Updated nodeset file and pre_conditions

--- spec/pre_conditions/site.pp
+++ spec/pre_conditions/site.pp
@@ -4,10 +4,11 @@ service { 'pe-puppetserver':
   ensure     => 'running',
   enable     => false,
   hasrestart => false, # Force Puppet to use start and stop to restart
-  start      => 'exit 0', # This will always work
-  stop       => 'exit 0', # This will also always work
+  start      => 'echo "Start"', # This will always work
+  stop       => 'echo "Stop"', # This will also always work
   hasstatus  => false, # Force puppet to use our command for status
-  status     => 'exit 0' # This will always exit 0 and therefor Puppet will think the service is running
+  status     => 'echo "Status"', # This will always exit 0 and therefor Puppet will think the service is running
+  provider   => 'base',
 }
 
 user { 'puppet':
