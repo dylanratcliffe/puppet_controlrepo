532eafff4fa2c4602f0f876ff1769184dfd1db33
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-11-16 19:20:11 +1100

    syntax

--- site/profile/manifests/vagrant.pp
+++ site/profile/manifests/vagrant.pp
@@ -10,7 +10,7 @@ class profile::vagrant {
 
   include ::archive
 
-  Package <| (title == 'faraday') or (title == 'faraday_middleware') |>
+  Package <| (title == 'faraday') or (title == 'faraday_middleware') |> {
     provider => 'puppet_gem',
   }
 
