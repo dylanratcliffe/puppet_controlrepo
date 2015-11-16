200bfb54ad91b2cead1edadeaa8ce12f15e1e2c2
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-11-16 17:05:43 +1100

    Removed custom ports

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -1,16 +1,16 @@
 class profile::jenkins {
   class { '::jenkins':
     version            => 'latest',
-    port               => 80,
+    #port               => 80,
     configure_firewall => true,
-    config_hash        => {
-      'HTTP_PORT' => { 'value' => '80' }
-    },
+    #config_hash        => {
+    #  'HTTP_PORT' => { 'value' => '80' }
+    #},
   }
 
-  firewall { '100 allow http  access':
-    dport  => 80,
-    proto  => tcp,
-    action => accept,
-  }
+  #firewall { '100 allow http  access':
+  #  dport  => 80,
+  #  proto  => tcp,
+  #  action => accept,
+  #}
 }
