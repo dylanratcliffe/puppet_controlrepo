58f89fedfa43b1e8596300148bbe5c71cfb7d0c4
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-01-18 12:54:52 +1100

    Added conssole config

--- site/profile/manifests/nc_proxy.pp
+++ site/profile/manifests/nc_proxy.pp
@@ -1,16 +1,24 @@
 class profile::nc_proxy {
-  package { 'puppetclassify-gem':
-    ensure   => latest,
-    name     => 'puppetclassify',
-    provider => 'gem',
-  }
-
   package { 'puppetclassify-puppet_gem':
     ensure   => latest,
     name     => 'puppetclassify',
     provider => 'puppet_gem',
   }
 
-
+  node_group { 'PE Console':
+    ensure               => 'present',
+    classes              => {
+      'pe_console_prune'                    => {
+        'prune_upto' => '30'},
+      'puppet_enterprise::license'          => {},
+      'puppet_enterprise::profile::console' => {
+        'console_services_api_listen_port'     => '44322',
+        'console_services_api_ssl_listen_port' => '44333'}},
+    environment          => 'production',
+    id                   => '80f54dde-e4c7-4c57-bcaa-6ede60ae7248',
+    override_environment => false,
+    parent               => 'PE Infrastructure',
+    rule                 => ['or', ['=', 'name', 'mom.puppetlabs.demo']],
+  }
 
 }
