4a4a2bb322ee1dfe519db796ce64827160b52560
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-01-18 12:38:56 +1100

    Added node manager and puppetclassify gems

---
+++ site/profile/manifests/nc_proxy.pp
@@ -0,0 +1,22 @@
+class profile::nc_proxy {
+  package { 'puppetclassify-gem':
+    ensure   => latest,
+    name     => 'puppetclassify',
+    provider => 'gem',
+  }
+
+  package { 'puppetclassify-puppet_gem':
+    ensure   => latest,
+    name     => 'puppetclassify',
+    provider => 'puppet_gem',
+  }
+
+  package { 'puppetclassify-puppetserver_gem':
+    ensure   => latest,
+    name     => 'puppetclassify',
+    provider => 'puppetserver_gem',
+  }
+
+  
+
+}
--- site/role/manifests/master_of_masters.pp
+++ site/role/manifests/master_of_masters.pp
@@ -1,3 +1,4 @@
 class role::master_of_masters {
   include profile::base
+  include profile::nc_proxy
 }
