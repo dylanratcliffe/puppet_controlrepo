b042ba883f0c5e4b83a4ea341c9f0aa6e46aa717
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-01-18 13:28:10 +1100

    Added some dependencies

--- site/profile/manifests/nc_proxy.pp
+++ site/profile/manifests/nc_proxy.pp
@@ -1,9 +1,11 @@
 class profile::nc_proxy {
-  package { 'puppetclassify-puppet_gem':
+  package { 'puppetclassify':
     ensure   => latest,
-    name     => 'puppetclassify',
     provider => 'puppet_gem',
   }
+
+  # Make sure that the gem is installed before we try to use it
+  Package['puppetclassify'] -> Node_group <||>
 
   node_group { 'PE Console':
     ensure               => 'present',
@@ -12,10 +14,9 @@ class profile::nc_proxy {
         'prune_upto' => '30'},
       'puppet_enterprise::license'          => {},
       'puppet_enterprise::profile::console' => {
-        'console_services_api_listen_port'     => '44322',
-        'console_services_api_ssl_listen_port' => '44333'}},
+        'console_services_api_listen_port'     => '4432',
+        'console_services_api_ssl_listen_port' => '4433'}},
     environment          => 'production',
-    id                   => '80f54dde-e4c7-4c57-bcaa-6ede60ae7248',
     override_environment => false,
     parent               => 'PE Infrastructure',
     rule                 => ['or', ['=', 'name', 'mom.puppetlabs.demo']],
