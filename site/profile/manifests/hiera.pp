f10105f6e8aee1f10f302c2306cd7db203f9d934
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-01-22 12:57:17 +1100

    Did some cleanup

--- site/profile/manifests/hiera.pp
+++ site/profile/manifests/hiera.pp
@@ -1,8 +1,8 @@
 class profile::hiera {
   class { '::hiera':
-    datadir    => '"/etc/puppetlabs/code/environments/%{environment}/hieradata"',
+    datadir   => '"/etc/puppetlabs/code/environments/%{environment}/hieradata"',
     #hiera_yaml => '/etc/puppetlabs/puppet/hiera.yaml',
-    hierarchy  => [
+    hierarchy => [
       '%{environment}',
       'common',
     ],
