091ade920e1589c2d4e241b15dfaf1523000099e
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-01-29 12:40:07 +1100

    Added git to base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,8 @@
 class profile::base {
-  package { ['tree','vim']:
+  package { ['tree','vim','git']:
     ensure => latest,
   }
+
+  # Make sure that we install git before we try to use it
+  Package['git'] -> Vcsrepo <| provider == 'git' |>
 }
