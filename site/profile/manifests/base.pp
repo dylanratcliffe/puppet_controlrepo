35542e554f6b03cf846149184d7250eae436cede
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-03-07 16:06:26 +1100

    Added epel deps

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,10 +1,11 @@
 class profile::base {
   include ::epel
-  
+
   package { ['tree','vim','git']:
     ensure => latest,
   }
 
   # Make sure that we install git before we try to use it
   Package['git'] -> Vcsrepo <| provider == 'git' |>
+  Class['::epel'] -> Package <| |>
 }
