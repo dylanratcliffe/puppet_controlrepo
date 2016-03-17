29a51ce663f0cce9be794b546654e895f389fec3
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-03-17 16:48:49 +1100

    Removed unnecissary stuff

--- site/profile/manifests/metrics/collectd.pp
+++ site/profile/manifests/metrics/collectd.pp
@@ -1,6 +1,5 @@
 class profile::metrics::collectd {
   $collectd_dir = '/etc/collectd'
-  $collectd_version = '5.1.0-1.el6.rft'
 
   # We need this repo because it has the latest version of collectd
   # other ones only have version 4 which does not support graphite
@@ -16,19 +15,9 @@ class profile::metrics::collectd {
 
   #require profile::metrics::collectd::compile
 
-  staging::file { 'collectd-5.5.0-1.el6.x86_64.rpm':
-    source => 'https://copr-be.cloud.fedoraproject.org/results/stevenmerrill/collectd5/epel-6-x86_64/collectd-5.5.0-1.el6/collectd-5.5.0-1.el6.x86_64.rpm',
-    before => Class['::collectd'],
-  }
 
   class { '::collectd':
     purge_config     => true,
-    package_ensure   => 'present',
-    package_provider => 'rpm',
-  }
-
-  Package <| title == 'collectd' |> {
-    source => '/opt/staging/profile/collectd-5.5.0-1.el6.x86_64.rpm',
   }
 
   include ::collectd::plugin::cpu
