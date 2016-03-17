ba716b35f3da4509065d3c97aff78e7f58ec9fd8
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-03-17 12:27:08 +1100

    Added code back in for compiling from source

--- site/profile/manifests/metrics/collectd.pp
+++ site/profile/manifests/metrics/collectd.pp
@@ -2,41 +2,23 @@ class profile::metrics::collectd {
   $collectd_dir = '/etc/collectd'
   $collectd_version = '5.1.0-1.el6.rft'
 
-  $dependencies = [
-    'perl',
-    'perl-devel',
-    'perl-CGI',
-    'perl-Collectd',
-    'perl-Config-General',
-    'perl-Time-HiRes',
-    'perl-URI',
-    'spamassassin',
-    'OpenIPMI-perl',
-    'lm_sensors',
-    'libstatgrab',
-    'libtool-ltdl',
-  ]
-
   # We need this repo because it has the latest version of collectd
   # other ones only have version 4 which does not support graphite
-  yumrepo { 'dag_testing_packages':
-    ensure   => present,
-    enabled  => '1',
-    gpgcheck => '0',
-    baseurl  => 'ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/$basearch/testing',
-  }
+  # yumrepo { 'dag_testing_packages':
+  #   ensure   => present,
+  #   enabled  => '1',
+  #   gpgcheck => '0',
+  #   baseurl  => 'ftp://fr2.rpmfind.net/linux/dag/redhat/el6/en/$basearch/testing',
+  # }
 
   # Do this yumrepo before ANY packages
   Yumrepo['dag_testing_packages'] -> Package <||>
 
-  package { $dependencies:
-    ensure => present,
-    before => Package['collectd'],
-  }
-
+  require profile::metrics::collectd::compile
+  
   class { '::collectd':
     purge_config   => true,
-    package_ensure => $collectd_version,
+    package_ensure => 'absent', # Changed to absent so that we don't manage it, usually $collectd_version
   }
 
   include ::collectd::plugin::cpu
---
+++ site/profile/manifests/metrics/collectd/compile.pp
@@ -0,0 +1,78 @@
+class profile::metrics::collectd::compile {
+  $collectd_version = '5.5.0'
+  $collectd_dir = '/etc/collectd'
+
+  $dependencies = [
+    'libatasmart-devel',
+    'libcurl-devel',
+    'libdbi-devel',
+    'libesmtp-devel',
+    'ganglia-devel',
+    'libgcrypt-devel',
+    'hal-devel',
+    'hiredis-devel',
+    'iptables-devel',
+    'java-1.8.0-openjdk-devel',
+    'openldap-devel',
+    'lvm2-devel',
+    'libmemcached-devel',
+    'libmnl-devel',
+    'libmodbus-devel',
+    'mysql-devel',
+    'net-snmp-devel',
+    'libnotify-devel',
+    'OpenIPMI-devel',
+    'liboping-devel',
+    'libpcap-devel',
+    'perl-devel',
+    'perl-ExtUtils-Embed',
+    'postgresql-devel',
+    'librabbitmq-devel',
+    'rrdtool-devel',
+    'lm_sensors-devel',
+    'libstatgrab-devel',
+    'libudev-devel',
+    'nut-devel',
+    'varnish-libs-devel',
+    'libvirt-devel',
+    'libxml2-devel',
+    'yajl-devel',
+    'protobuf-c-devel',
+    'python-devel',
+  ]
+
+  require ::gcc
+
+  package { $dependencies:
+    ensure => present,
+  }
+
+  file { $collectd_dir:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  staging::deploy { "collectd-${collectd_version}.tar.bz2":
+    target  => $collectd_dir,
+    source  => "http://collectd.org/files/collectd-${collectd_version}.tar.bz2",
+    require => File[$collectd_dir],
+  }
+
+  exec { 'compile_collectd':
+    command => 'configure',
+    cwd     => "${collectd_dir}/collectd-${collectd_version}",
+    path    => "${::path}:${collectd_dir}/collectd-${collectd_version}",
+    creates => "${collectd_dir}/collectd-${collectd_version}/config.status",
+    require => Staging::Deploy["collectd-${collectd_version}.tar.bz2"],
+  }
+
+  exec { 'install_collectd':
+    command => 'make all install',
+    path    => "${::path}:${collectd_dir}/collectd-${collectd_version}",
+    creates => '/opt/collectd',
+    require => [Exec['compile_collectd'],Package[$dependencies]],
+  }
+
+}
