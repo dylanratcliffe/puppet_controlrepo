da7731f14e7cca2dbe9f61b507b703c74339f1cb
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-04-04 21:01:12 +1000

    Removed debugging

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -1,11 +1,8 @@
 class profile::puppetmaster {
-  # Wait until we have installed the stuff first
-  $query_results = query_resources("Class['profile::puppetmaster']","Package['dslkjfng']")
-  #if $query_results {
-    #include profile::puppetmaster::tuning
-  #}
-  $count = count($query_results)
-  notify { "${count}": }
+  # Wait until we have installed the stuff first before including this class
+  if count(query_resources("Class['profile::puppetmaster']","Package['puppetclassify']")) > 0 {
+    include profile::puppetmaster::tuning
+  }
 
   firewall { '100 allow https access':
     dport  => 443,
