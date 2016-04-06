b43b1353a0ad95d88340405e128c83ff51f7af51
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-04-06 23:57:28 +1000

    Commented out java version

--- site/profile/manifests/bamboo.pp
+++ site/profile/manifests/bamboo.pp
@@ -2,7 +2,7 @@ class profile::bamboo {
   class { '::bamboo':
     username       => 'bamboo',
     pass_hash      => '4b9c4b7a4a362c2ca77d9d0ba68952eec1f1f51772b74f4e08fdc2c40b2ccc9f33c2f1db0f4f64514f299eec6d641799d19b8da92b139fa7abc4719ea148d2ac',
-    bamboo_version => '5.10.0',
+    #bamboo_version => '5.10.0',
     bamboo_home    => '/home/bamboo/data',
     bamboo_data    => '/var/bamboo/data',
     java_manage    => true,
