df1d0e17b540abd53f500deabf2bda32f1f57f35
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-05-26 14:55:26 +1000

    Added collection of exported haproxy

--- site/profile/manifests/compile/balancer.pp
+++ site/profile/manifests/compile/balancer.pp
@@ -2,7 +2,7 @@ class profile::compile::balancer {
   include ::haproxy
 
   haproxy::listen { 'puppet00':
-    collect_exported => false,
+    collect_exported => true,
     ipaddress        => $::ipaddress,
     ports            => '8140',
   }
