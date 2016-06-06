79367e4fdc0deabf9dab56108506a63125970e83
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-06-06 17:17:05 +1000

    Uncommented AWS

--- site/profile/manifests/aws_nodes.pp
+++ site/profile/manifests/aws_nodes.pp
@@ -2,21 +2,22 @@
 class profile::aws_nodes {
   ec2_instance { 'agent-1':
     ensure            => 'running',
-    # availability_zone => 'ap-southeast-2c',
-    # block_devices     => [
-    #   {
-    #     'delete_on_termination' => true,
-    #     'device_name'           => '/dev/sda1',
-    #     'volume_size'           => 10,
-    #   }
-    # ],
-    # ebs_optimized     => false,
+    availability_zone => 'ap-southeast-2c',
+    block_devices     => [
+      {
+        'delete_on_termination' => true,
+        'device_name'           => '/dev/sda1',
+        'volume_size'           => 10,
+      }
+    ],
+    ebs_optimized     => false,
     image_id          => 'ami-e0c19f83',
     instance_type     => 't2.micro',
     key_name          => 'personal_aws',
-    # monitoring        => false,
+    monitoring        => false,
     region            => 'ap-southeast-2',
-    # security_groups   => ['default'],
+    security_groups   => ['default'],
+    vpc               => 'default',
     user_data         => epp('profile/userdata.epp',{
       'master_ip'   => $::ec2_metadata['public-ipv4'],
       'master_fqdn' => $::networking['fqdn'],
