b243d5b6a5a4f6f4a9f13ff4b9809b7222e6c930
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-06-06 17:11:53 +1000

    Change hier_yaml

--- site/profile/manifests/hiera.pp
+++ site/profile/manifests/hiera.pp
@@ -1,8 +1,8 @@
 class profile::hiera {
   class { '::hiera':
-    datadir   => '"/etc/puppetlabs/code/environments/%{environment}/hieradata"',
-    #hiera_yaml => '/etc/puppetlabs/puppet/hiera.yaml',
-    hierarchy => [
+    datadir    => '"/etc/puppetlabs/code/environments/%{environment}/hieradata"',
+    hiera_yaml => '/etc/puppetlabs/puppet/hiera.yaml',
+    hierarchy  => [
       '%{environment}',
       'common',
     ],
