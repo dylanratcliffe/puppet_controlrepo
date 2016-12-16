2af69c1b9addb982e2777e42bb0ebe032da7b247
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-12-16 15:25:27 +1100

    Updated mysql stuff

--- site/profile/manifests/mysql_server.pp
+++ site/profile/manifests/mysql_server.pp
@@ -9,6 +9,29 @@ class profile::mysql_server {
     content => "This is a mysql server\nManaged by Puppet\n",
   }
 
+  # Create the databases
+  mysql_database { ['customers','products']:
+    ensure  => present,
+    require => Class['mtsql::server']
+  }
+
+  $mysql_users = [
+    'dylan',
+    'joe',
+    'karl',
+  ]
+
+  # Create the users
+  mysql_user { $mysql_users:
+    ensure  => present,
+    require => Class['mtsql::server']
+  }
+
+  # Purge any extra users or databases
+  resources { ['mysql_database','mysql_user']:
+    purge => true,
+  }
+
   unless $::kernel == 'linux' {
     fail('The profile::mysql_server profile cannot be used on non-linux systems')
   }
