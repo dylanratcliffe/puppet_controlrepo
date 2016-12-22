c52c56e44cc352f7ad2c57313340a5aad02a935e
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-12-22 17:30:52 +1100

    Added debugging

--- site/console/functions/user/token.pp
+++ site/console/functions/user/token.pp
@@ -3,8 +3,10 @@ function console::user::token (
 ) {
   include ::console
   if find_file("${::console::token_dir}/${name}") {
+    notice "found file, reading"
     file("${::console::token_dir}/${name}")
   } else {
+    notice "Didnt find file @ ${::console::token_dir}/${name}"
     ''
   }
 }
