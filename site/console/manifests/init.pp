c8fbe1189d3f1ae90823c8f57fc0cac7db5a55e6
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-12-22 17:12:43 +1100

    Make sure that the token_dir only exists on master

--- site/console/manifests/init.pp
+++ site/console/manifests/init.pp
@@ -3,10 +3,12 @@
 class console (
   $token_dir = '/etc/puppetlabs/puppet/user_tokens',
 ) {
-  file { $token_dir:
-    ensure => directory,
-    owner  => 'pe-puppet',
-    group  => 'pe-puppet',
-    mode   => '0700',
+  if $::pe_build {
+    file { $token_dir:
+      ensure => directory,
+      owner  => 'pe-puppet',
+      group  => 'pe-puppet',
+      mode   => '0700',
+    }
   }
 }
