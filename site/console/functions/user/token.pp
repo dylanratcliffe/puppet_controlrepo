7345405af195fcb70f78589bb9c0460101a642f7
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-12-22 17:48:23 +1100

    Added debugging

--- site/console/functions/user/token.pp
+++ site/console/functions/user/token.pp
@@ -3,6 +3,7 @@ function console::user::token (
 ) {
   include ::console
   if find_file("${::console::token_dir}/${name}") {
+    notice regsubst(file("${::console::token_dir}/${name}"),/\n$/,'')
     regsubst(file("${::console::token_dir}/${name}"),/\n$/,'')
   } else {
     undef
