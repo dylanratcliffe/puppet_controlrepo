e447c17465cc673c8686a4027874f78a695ea4ca
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-05-04 13:44:41 +0800

    Fixed version number

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -1,6 +1,6 @@
 class profile::jenkins {
   class { '::jenkins':
-    version            => '2.53',
+    version            => '2.53-1.1',
     service_enable     => false,
     configure_firewall => true,
     executors          => $::processors['count'],
