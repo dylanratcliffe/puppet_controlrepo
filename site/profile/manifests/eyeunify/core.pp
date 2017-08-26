c9d3a8c243d6077252b0e9639dc2c8b56a0bffb6
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-08-26 13:58:05 +1000

    Alignment

--- site/profile/manifests/eyeunify/core.pp
+++ site/profile/manifests/eyeunify/core.pp
@@ -19,7 +19,7 @@ class profile::eyeunify::core {
   }
 
   wildfly::config::user_roles { 'admin':
-    roles   => 'administrator,operator',
+    roles => 'administrator,operator',
   }
 
   wildfly::config::user { 'guest':
@@ -29,6 +29,6 @@ class profile::eyeunify::core {
   }
 
   wildfly::config::user_roles { 'guest':
-    roles   => 'administrator,operator',
+    roles => 'administrator,operator',
   }
 }
