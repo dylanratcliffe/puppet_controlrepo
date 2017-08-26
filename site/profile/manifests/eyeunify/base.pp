862e1c0a0a279b4014e53b07d5497cabb9ba489e
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-08-26 13:27:58 +1000

    Changed management bind

--- site/profile/manifests/eyeunify/base.pp
+++ site/profile/manifests/eyeunify/base.pp
@@ -6,10 +6,20 @@ class profile::eyeunify::base {
   }
 
   class { '::wildfly':
-    java_home => '/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.141-1.b16.el7_3.x86_64/jre',
-    mgmt_user => {
+    java_home      => '/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.141-1.b16.el7_3.x86_64/jre',
+    external_facts => true,
+    mgmt_user      => {
       'username' => 'wildfly',
       'password' => 'hunter2',
+    },
+    properties     => {
+      'jboss.bind.address'            => '0.0.0.0',
+      'jboss.bind.address.management' => '0.0.0.0',
+      'jboss.management.http.port'    => '9990',
+      'jboss.management.https.port'   => '9993',
+      'jboss.http.port'               => '8080',
+      'jboss.https.port'              => '8443',
+      'jboss.ajp.port'                => '8009',
     },
   }
 
