97573283bb4074b4ac3412661212c4ca0a9864a5
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-09-09 18:20:40 +1000

    Re-ordered, this shouldnt have any effect

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -61,49 +61,59 @@ class profile::puppetmaster {
     description => 'Can run Puppet, deploy code and use PuppetDB',
     permissions => [
       {
-        'action'      => 'run',
+        'action'      => 'view',
         'instance'    => '*',
-        'object_type' => 'puppet_agent'
-      }, {
-        'action'      => 'modify_children',
-        'instance'    => '*',
-        'object_type' => 'node_groups'
-      }, {
-        'action'      => 'edit_child_rules',
-        'instance'    => '*',
-        'object_type' => 'node_groups'
-      }, {
+        'object_type' => 'node_groups',
+      },
+      {
         'action'      => 'deploy_code',
         'instance'    => '*',
-        'object_type' => 'environment'
-      }, {
-        'action'      => 'accept_reject',
+        'object_type' => 'environment',
+      },
+      {
+        'action'      => 'modify_children',
         'instance'    => '*',
-        'object_type' => 'cert_requests'
-      }, {
-        'action'      => 'edit_params_and_vars',
+        'object_type' => 'node_groups',
+      },
+      {
+        'action'      => 'edit_child_rules',
         'instance'    => '*',
-        'object_type' => 'node_groups'
-      }, {
-        'action'      => 'edit_classification',
-        'instance'    => '*',
-        'object_type' => 'node_groups'
-      }, {
-        'action'      => 'view',
-        'instance'    => '*',
-        'object_type' => 'node_groups'
-      }, {
+        'object_type' => 'node_groups',
+      },
+      {
         'action'      => 'view_data',
         'instance'    => '*',
-        'object_type' => 'nodes'
-      }, {
+        'object_type' => 'nodes',
+      },
+      {
+        'action'      => 'edit_classification',
+        'instance'    => '*',
+        'object_type' => 'node_groups',
+      },
+      {
         'action'      => 'view',
         'instance'    => '*',
-        'object_type' => 'console_page'
-      }, {
+        'object_type' => 'console_page',
+      },
+      {
+        'action'      => 'accept_reject',
+        'instance'    => '*',
+        'object_type' => 'cert_requests',
+      },
+      {
         'action'      => 'set_environment',
         'instance'    => '*',
-        'object_type' => 'node_groups'
+        'object_type' => 'node_groups',
+      },
+      {
+        'action'      => 'run',
+        'instance'    => '*',
+        'object_type' => 'puppet_agent',
+      },
+      {
+        'action'      => 'edit_params_and_vars',
+        'instance'    => '*',
+        'object_type' => 'node_groups',
       },
     ],
   }
