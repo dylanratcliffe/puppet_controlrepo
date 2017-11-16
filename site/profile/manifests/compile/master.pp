70b408351f2250cac5788d643a1277601d77f509
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-11-16 11:26:58 +1100

    Added PXP listening service

--- site/profile/manifests/compile/master.pp
+++ site/profile/manifests/compile/master.pp
@@ -2,10 +2,18 @@ class profile::compile::master (
   String $listening_pool = 'puppet00',
 ) {
   @@haproxy::balancermember { $::fqdn:
-    listening_service => $listening_pool,
+    listening_service => "${listening_pool}-8140",
     server_names      => $::fqdn,
     ipaddresses       => $::networking['ip'],
     ports             => '8140',
+    options           => 'check',
+  }
+
+  @@haproxy::balancermember { $::fqdn:
+    listening_service => "${listening_pool}-8142",
+    server_names      => $::fqdn,
+    ipaddresses       => $::networking['ip'],
+    ports             => '8142',
     options           => 'check',
   }
 }
