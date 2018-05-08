179d3f9193a51f81c58d9a64b68f828f0e03f113
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-05-08 18:02:27 +1000

    Added encrypted data

--- site/profile/manifests/puppetmaster/autosign.pp
+++ site/profile/manifests/puppetmaster/autosign.pp
@@ -1,19 +1,23 @@
 class profile::puppetmaster::autosign (
   String $logfile     = '/var/log/puppetlabs/puppetserver/autosign.log',
   String $journalfile = '/etc/puppetlabs/puppetserver/autosign.journal',
+  String $password    = undef,
 ) {
   class { '::autosign':
     ensure   => 'latest',
     settings => {
-      'general'   => {
+      'general'       => {
         'loglevel' => 'INFO',
         'logfile'  => $logfile,
       },
-      'jwt_token' => {
+      'jwt_token'     => {
         'secret'      => fqdn_rand_string(10),
         'validity'    => '7200',
         'journalfile' => $journalfile,
-      }
+      },
+      'password_list' => {
+        'password' => $password,
+      },
     },
   }
 
@@ -25,30 +29,4 @@ class profile::puppetmaster::autosign (
     notify  => Service['pe-puppetserver'],
     require => Class['::autosign'],
   }
-
-  #
-  #
-  # file { '/etc/puppetlabs/puppet/autosign':
-  #   ensure => directory,
-  #   owner  => 'pe-puppet',
-  #   group  => 'pe-puppet',
-  #   mode   => '0700',
-  # }
-  #
-  # file { '/etc/puppetlabs/puppet/autosign/autosign.sh':
-  #   ensure => file,
-  #   owner  => 'pe-puppet',
-  #   group  => 'pe-puppet',
-  #   mode   => '0700',
-  #   source => 'puppet:///modules/profile/autosign.sh',
-  # }
-  #
-  # ini_setting { 'policy_based_autosigning':
-  #   ensure  => present,
-  #   path    => '/etc/puppetlabs/puppet/puppet.conf',
-  #   section => 'master',
-  #   setting => 'autosign',
-  #   value   => '/etc/puppetlabs/puppet/autosign/autosign.sh',
-  #   notify  => Service['pe-puppetserver'],
-  # }
 }
