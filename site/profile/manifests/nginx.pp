602753fe4f44433e472b03e142662c9c9bd2e45c
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-05-09 10:47:11 +1000

    Updated nginx to remove default

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -1,3 +1,10 @@
 class profile::nginx {
   include ::nginx
+
+  file { 'default_config_file':
+    ensure  => absent,
+    path    => "${nginx::conf_dir}/conf.d/default.conf",
+    require => Class['nginx::config'],
+    notify  => Class['nginx::service'],
+  }
 }
