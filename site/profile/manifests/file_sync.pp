a40b21fe91925e6b0d1bf0e0820255fedbf57718
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-10-01 14:17:56 +0800

    Added file sync role

---
+++ site/profile/manifests/file_sync.pp
@@ -0,0 +1,12 @@
+# Sets up file sync on an arbitrary host
+#
+class profile::file_sync {
+  class { 'puppet_enterprise::master::file_sync':
+    puppet_master_host                        => $puppet_enterprise::puppet_master_host,
+    master_of_masters_certname                => $puppet_enterprise::puppet_master_host,
+    localcacert                               => $puppet_enterprise::params::localcacert,
+    puppetserver_jruby_puppet_master_code_dir => '/etc/puppetlabs/code',
+    puppetserver_webserver_ssl_port           => '8140',
+    storage_service_disabled                  => false,
+  }
+}
---
+++ site/role/manifests/file_sync.pp
@@ -0,0 +1,4 @@
+class role::file_sync {
+  include profile::base
+  include role::file_sync
+}
