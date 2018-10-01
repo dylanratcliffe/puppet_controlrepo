ffac04663c9e1225aeca5fe865f255f43440786c
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-10-01 16:55:15 +0800

    Added puppet serverice which is rewuired

--- site/profile/manifests/file_sync.pp
+++ site/profile/manifests/file_sync.pp
@@ -5,6 +5,10 @@ class profile::file_sync {
 
   puppet_enterprise::trapperkeeper::pe_service { 'puppetserver': }
 
+  puppet_enterprise::trapperkeeper::bootstrap_cfg { 'jruby-puppet-pooled-service' :
+    namespace => 'puppetlabs.services.jruby.jruby-puppet-service',
+  }
+
   class { 'puppet_enterprise::master::file_sync':
     puppet_master_host                        => $puppet_enterprise::puppet_master_host,
     master_of_masters_certname                => $puppet_enterprise::puppet_master_host,
