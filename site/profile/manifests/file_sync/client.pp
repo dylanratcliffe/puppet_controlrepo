b9f3be7bf53636110fcd4a4b0478f06bc0783894
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-10-02 13:55:03 +0800

    Fixed naming

--- site/profile/manifests/file_sync/client.pp
+++ site/profile/manifests/file_sync/client.pp
@@ -12,7 +12,7 @@
 # @param code_dir Where to sync the code
 # @param java_args Java arguments for the pe-puppetserver process
 #
-class profile::file_sync (
+class profile::file_sync::client (
   $puppetserver_conf_dir = '/etc/puppetlabs/puppetserver/conf.d',
   $enable_gc_logging     = true,
   $code_dir              = '/etc/puppetlabs/code',
