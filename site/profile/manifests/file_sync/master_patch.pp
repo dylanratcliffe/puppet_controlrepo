c67d28d67798ee949bcdc2a402c69772446acbf9
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-10-02 14:55:38 +0800

    testing defult as undef

--- site/profile/manifests/file_sync/master_patch.pp
+++ site/profile/manifests/file_sync/master_patch.pp
@@ -11,7 +11,7 @@ class profile::file_sync::master_patch {
                       ['and', ['=', 'type', 'Class'],
                       ['=', 'title', 'Puppet_enterprise::Profile::Master'],
                       ['=', ['node','active'], true]]]])) |$master| { $master['certname'] }
-  $whitelisted_certnames = lookup('puppet_enterprise::master::file_sync::whitelisted_certnames', {'default_value' => []})
+  $whitelisted_certnames = lookup('puppet_enterprise::master::file_sync::whitelisted_certnames', {'default_value' => undef})
   $authorized_certs      = pe_union([$facts['certname']], $whitelisted_certnames)
   $certs_authorized_to_communicate_with_file_sync = pe_sort(pe_unique(pe_union($authorized_certs, $masters_in_puppetdb)))
 
