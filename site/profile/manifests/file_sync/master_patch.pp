a8828d7ea987ad60526ac2e0f58754e6df22b218
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-10-02 14:01:09 +0800

    Changed override method to second config file to avoid parse order issues

--- site/profile/manifests/file_sync/master_patch.pp
+++ site/profile/manifests/file_sync/master_patch.pp
@@ -1,7 +1,19 @@
 # This class patches the Puppet MoMs to allow all nodes that are able to access
 # the file-sync API to also be clients
 class profile::file_sync::master_patch {
-  Pe_hocon_setting <| title == 'file-sync.client-certnames' |> {
-    value => $puppet_enterprise::master::file_sync::certs_authorized_to_communicate_with_file_sync,
+  file { "${puppet_enterprise::master::file_sync::confdir}/conf.d/99_file_sync_override.conf":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['pe-puppetserver'],
+  }
+
+  pe_hocon_setting { 'override file-sync.client-certnames':
+    path    => "${puppet_enterprise::master::file_sync::confdir}/conf.d/99_file_sync_override.conf",
+    setting => 'file-sync.client-certnames',
+    value   => $$puppet_enterprise::master::file_sync::certs_authorized_to_communicate_with_file_sync,
+    type    => 'array',
+    notify  => Service['pe-puppetserver'],
   }
 }
