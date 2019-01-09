b8e14e40363a7c6ee3e1dc88bb95fbf9c19e9dde
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2019-01-09 15:44:16 +0000

    Added auto provision script

--- site/profile/manifests/cd4pe/artifactory.pp
+++ site/profile/manifests/cd4pe/artifactory.pp
@@ -24,14 +24,6 @@ class profile::cd4pe::artifactory (
     source => 'puppet:///modules/profile/artifactory/config_descriptor.xml',
   }
 
-  file { "${bootstrap_dir}/security.import.xml":
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0600',
-    source => 'puppet:///modules/profile/artifactory/security_descriptor.xml',
-  }
-
   # Start a quick alpine container to copt files around
   $docker_command_prefix = "docker run --rm -v ${bootstrap_dir}:/source -v data_s3:/dest -w /source alpine"
 
@@ -43,22 +35,19 @@ class profile::cd4pe::artifactory (
     subscribe   => File["${bootstrap_dir}/artifactory.config.import.xml"],
   }
 
-  # ['artifactory.config.import.xml', 'security.import.xml'].each |$file| {
-  ['artifactory.config.import.xml'].each |$file| {
-    exec { "move ${file} into data_s3":
-      command     => "${docker_command_prefix} cp /source/${file} /dest/etc/${file}",
-      path        => $facts['path'],
-      refreshonly => true,
-      require     => Exec['create /etc inside data_s3'],
-      subscribe   => File["${bootstrap_dir}/artifactory.config.import.xml"],
-    }
+  exec { 'move artifactory.config.import.xml into data_s3':
+    command     => "${docker_command_prefix} cp /source/artifactory.config.import.xml /dest/etc/artifactory.config.import.xml",
+    path        => $facts['path'],
+    refreshonly => true,
+    require     => Exec['create /etc inside data_s3'],
+    subscribe   => File["${bootstrap_dir}/artifactory.config.import.xml"],
   }
 
   exec { 'set permissions':
     command     => "${docker_command_prefix} chown -R 1030:1030 /dest",
     path        => $facts['path'],
     refreshonly => true,
-    subscribe   => File["${bootstrap_dir}/artifactory.config.import.xml","${bootstrap_dir}/security.import.xml"],
+    subscribe   => File["${bootstrap_dir}/artifactory.config.import.xml"],
   }
 
   docker::image { 'docker.bintray.io/jfrog/artifactory-oss':
---
+++ site/profile/manifests/cd4pe/connection.pp
@@ -0,0 +1,49 @@
+class profile::cd4pe::connection (
+  Sensitive[String] $license,
+  String            $artifactory_user     = 'admin',
+  Sensitive[String] $artifactory_password = Sensitive('password'),
+  String            $artifactory_endpoint = "${facts['fqdn']}:8081",
+  String            $cd4pe_endpoint       = "${facts['fqdn']}:8080",
+  String            $cd4pe_root_login     = 'noreply@puppet.com',
+  Sensitive[String] $cd4pe_root_pw        = Sensitive('puppetlabs'),
+  String            $cd4pe_dump           = "${facts['fqdn']}:7000",
+  String            $cd4pe_backend        = "${facts['fqdn']}:8000",
+) {
+  # Ensure that all setup is done before this is completed
+  require profile::cd4pe
+  require profile::cd4pe::artifactory
+
+  # Create a folder for these files
+  file { '/etc/cd4pe':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700',
+  }
+
+  # Drop the license file
+  file { '/etc/cd4pe/license.json':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    content => $license,
+  }
+
+  file { '/etc/cd4pe/connection_script.sh':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => epp('profile/cd4pe/connection_script.sh.epp', {
+      'artifactory_user'     => $artifactory_user,
+      'artifactory_password' => $artifactory_password,
+      'artifactory_endpoint' => $artifactory_endpoint,
+      'cd4pe_endpoint'       => $cd4pe_endpoint,
+      'cd4pe_root_login'     => $cd4pe_root_login,
+      'cd4pe_root_pw'        => $cd4pe_root_pw,
+      'cd4pe_dump'           => $cd4pe_dump,
+      'cd4pe_backend'        => $cd4pe_backend,
+    })
+  }
+}
