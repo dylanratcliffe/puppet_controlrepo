64412ff9e8bf554f178e2356a2eb9b36fb2b9ce2
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2019-12-16 11:57:57 +0000

    Update all modules

--- site/profile/manifests/eyeunify/exec.pp
+++ site/profile/manifests/eyeunify/exec.pp
@@ -1,10 +1,9 @@
 # == Class: profile::eyeunify::exec
 #
 class profile::eyeunify::exec {
-  java::oracle { 'jre8' :
-    ensure  => 'present',
-    version => '8',
-    java_se => 'jre',
+  class { 'java' :
+    version      => '8',
+    distribution => 'jre',
   }
 
   # TODO: This is a work in progress
