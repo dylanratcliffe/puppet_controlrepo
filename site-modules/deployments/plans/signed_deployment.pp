84d9127bf22f42cfaa4a3dadbd8849d679615063
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2020-11-25 15:44:19 +0000

    Updated refs

--- site-modules/deployments/plans/signed_deployment.pp
+++ site-modules/deployments/plans/signed_deployment.pp
@@ -98,8 +98,8 @@ plan deployments::signed_deployment (
       'deployment_signature::file_sync_commit',
       $deployment_server,
       {
-        'message'      => "Deployed with a valid signature and approval dated: ${deployment_info.dig('approval', 'result', 'approvalDecisionDate')}",
-        'name'         => "${deployment_info.dig('approval', 'result', 'approverUsername')}",
+        'message'      => "Deployed with a valid signature and approval dated: ${signature_data.dig('approval', 'result', 'approvalDecisionDate')}",
+        'name'         => "${signature_data.dig('approval', 'result', 'approverUsername')}",
         'email'        => 'NA',
         'submodule_id' => $deployment_info['node_group_environment'],
       }
