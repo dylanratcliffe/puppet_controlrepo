039cfcebfa355c735dfd7100e2f46697f8d2af93
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2021-02-09 12:52:21 +0000

    Removed collectd

--- site-modules/role/manifests/puppetmaster/basic.pp
+++ site-modules/role/manifests/puppetmaster/basic.pp
@@ -1,7 +1,7 @@
 class role::puppetmaster::basic {
   include ::profile::base
   include ::profile::puppetmaster
-  include ::profile::metrics::collectd
+  # include ::profile::metrics::collectd
   include ::profile::puppetmaster::autosign
   include ::pe_repo::platform::windows_i386
   include ::pe_repo::platform::windows_x86_64
