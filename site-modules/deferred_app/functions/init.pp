6877e218daa8f8e97f647af5c64b3e218b9ebb41
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2021-06-29 15:18:14 +0100

    Initial test

---
+++ site-modules/deferred_app/functions/init.pp
@@ -0,0 +1,21 @@
+function deferred_epp(
+  String $template,
+  Hash   $options,
+) {
+  # First thing that we would need to do would be to find all possible locations
+  # that the module could be. This includes:
+  #
+  # - basemodulepath
+  # - environmentpath
+  $module_locations = [
+    $settings::basemodulepath,
+    $settings::modulepath,
+  ]
+
+  # Read the template from the Puppetserver and store it in a variable so that it can be passed in the catalog
+  # $template_contents = file("")
+
+  $module_locations.each |$path| {
+    notify { $path: }
+  }
+}
